a7a0c738362151b07d3e6af818ea92f577890ec4
Mateo Maturana <48253525+maturanamateo@users.noreply.github.com>
2023-02-08 15:11:50 -0500

    Initial commit

---
+++ part1/src/lowercase.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mystring.h"
+
+/**
+ * Take an array of char* pointers and print each of the strings to standard
+ * out. This function must use only pointer arithmetic and no array
+ * subscripting. The output start with "[", ends with "]\n", and prints the
+ * strings inside, each separated by a comma and space.
+ * Example: [Hi, BYE, AP, COMSW 3157, FunTimes]
+ */
+void display_strings(char **strings) {
+    printf("[");
+    if (strings != NULL) {
+        char **str_ptr = strings;
+        if (*str_ptr != NULL) {
+            printf("%s", *str_ptr++);
+            while (*str_ptr != NULL) {
+                printf(", %s", *str_ptr++);
+            }
+        }
+    }
+    printf("]\n");
+}
+
+static char **copy_args_lowercase(int argc, char **argv) {
+    return NULL;
+}
+
+static void free_copy(char **copy) {
+    return;
+}
+
+
+/**
+ * DO NOT MODIFY main()
+ */
+int main(int argc, char **argv) {
+    char **copy;
+    copy = copy_args_lowercase(argc, argv);
+
+    printf("Raw arguments       : ");
+    display_strings(argv + 1);
+
+    printf("Lowercase arguments : ");
+    display_strings(copy);
+
+    free_copy(copy);
+
+    return EXIT_SUCCESS;
+}
